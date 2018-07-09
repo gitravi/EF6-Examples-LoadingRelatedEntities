1f124a283665770f3a29ca3d2da50238863d7289
gitravi <ravisen2011@gmail.com>
2018-07-09 17:25:04 +0530

    Created demo app for EF 6 Eager/lazy/Explicit loading.

---
+++ EF6LoadingRelatedEntitiesDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6LoadingRelatedEntitiesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /*  Pre-requisites:
+                 *  => Before execution please change the connection string to your SQL Server Database.
+                 *  => Restore nuget packages
+                 */
+
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                using (SchoolContext context = new SchoolContext(connectionString))
+                {
+                    #region Print generated query to console
+                    //context.Database.Log = (q) => Console.WriteLine(q);
+                    #endregion
+
+                    #region Lazy Loading
+                    //var query = context.Standards.Include(b => b.Students);
+                    #endregion
+
+                    #region Eager Loading
+                    //var query = context.Standards.Include(b => b.Students).ToList();
+                    //int StandardCountFromSeed = 5;
+                    //for (int i = 1; i <= StandardCountFromSeed; i++)
+                    //{
+                    //    var standard = query.FirstOrDefault(m => m.Id == i);
+                    //    foreach (var student in standard.Students)
+                    //    {
+                    //        Console.WriteLine($"Student Id: {student.Id} , Name: {student.Name} ,Standard: {standard.Name}");
+                    //    }
+                    //}
+                    #endregion
+
+                    #region Explicit Loading
+                    var _standard = context.Standards.FirstOrDefault(i => i.Id == 2);
+                    context.Entry(_standard).Collection(s => s.Students).Load();
+                    foreach (var student in _standard.Students)
+                    {
+                        Console.WriteLine($"Student Id: {student.Id} , Name: {student.Name} ,Standard: {_standard.Name}");
+                    }
+                    #endregion
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ EF6LoadingRelatedEntitiesDemo/SchoolContext.cs
@@ -0,0 +1,58 @@
+﻿using EF6LoadingRelatedEntitiesDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6LoadingRelatedEntitiesDemo
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(string connectionString) : base(connectionString)
+        {
+            Database.SetInitializer<SchoolContext>(new SchoolDBInitializer());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+
+    public class SchoolDBInitializer : CreateDatabaseIfNotExists<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            List<Standard> defaultStandards = new List<Standard>();
+            defaultStandards.Add(new Standard
+            {
+                Id = 1,
+                Name = "First",
+                Students = new List<Student> {
+                new Student{Id=1, Name="Student 1"},
+                new Student{Id=2, Name="Student 2"},
+                new Student{Id=3, Name="Student 3"},
+                new Student{Id=4, Name="Student 4"},
+                new Student{Id=5, Name="Student 5"},
+            }
+            });
+            defaultStandards.Add(new Standard
+            {
+                Id = 2,
+                Name = "Second",
+                Students = new List<Student> {
+                new Student{Id=6, Name="Student 6"},
+                new Student{Id=7, Name="Student 7"},
+                new Student{Id=8, Name="Student 8"},
+                new Student{Id=9, Name="Student 9"},
+                new Student{Id=10, Name="Student 10"},
+            }
+            });
+            defaultStandards.Add(new Standard { Id = 3, Name = "Second" });
+            defaultStandards.Add(new Standard { Id = 4, Name = "Third" });
+            defaultStandards.Add(new Standard { Id = 5, Name = "Fourth" });
+            context.Standards.AddRange(defaultStandards);
+            base.Seed(context);
+        }
+    }
+}
